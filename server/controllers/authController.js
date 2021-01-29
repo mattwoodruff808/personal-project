0c2aafea62aef098f365d4ac4c657e3debdedca4
mattwoodruff808 <mjwoodruff93@gmail.com>
2021-01-29 10:46:11 -0700

    Finished authController.js

---
+++ server/controllers/authController.js
@@ -0,0 +1,44 @@
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+    register: async (req, res) => {
+        const {email, username, password} = req.body;
+        const db = req.app.get('db');
+
+        const [foundUser] = await db.user.check_user(email);
+        if (foundUser){
+            return res.status(400).send('Email already in use');
+        }
+
+        let salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(password, salt);
+
+        const [newUser] = await db.user.register_user([email, username, hash, `https://robohash.org/${username}.png`]);
+
+        req.session.user = newUser;
+        res.status(201).send(req.session.user);
+    },
+    login: async (req, res) => {
+        const {email, password} = req.body;
+        const db = req.app.get('db');
+
+        const [foundUser] = await db.user.check_user(email);
+        if (!foundUser){
+            return res.status(400).send('Email not found');
+        }
+
+        const authenticated = bcrypt.compareSync(password, foundUser.password);
+        if (!authenticated){
+            return res.status(401).send('Password is incorrect');
+        }
+
+        delete foundUser.password;
+
+        req.session.user = foundUser;
+        res.status(202).send(req.session.user);
+    },
+    logout: (req, res) => {
+        req.session.destroy();
+        res.sendStatus(200);
+    }
+}
