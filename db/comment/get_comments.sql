1a77f896e3f31656ce6875fa7b190cfedee69b17
mattwoodruff808 <mjwoodruff93@gmail.com>
2021-02-08 10:55:59 -0700

    Finished getComment/addComment on Comments.js

--- db/comment/get_comments.sql
+++ db/comment/get_comments.sql
@@ -1,3 +1,4 @@
-SELECT * FROM comment
+SELECT c.comment_id AS comment_id, c.content AS content, c.date_created AS date_created, c.user_id AS user_id, c.recipe_id AS recipe_id, u.profile_pic AS profile_pic, u.username AS username FROM comment c
+JOIN auth_user u ON c.user_id = u.user_id
 WHERE recipe_id = $1
-ORDER BY DESC;
+ORDER BY date_created DESC;
--- src/Components/Recipe/Comments/Comments.js
+++ src/Components/Recipe/Comments/Comments.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import {useState, useEffect} from 'react';
 import {connect} from 'react-redux';
 import {getRecipe} from '../../../redux/recipeReducer';
@@ -5,18 +6,98 @@ import './Comments.css';
 
 const Comments = (props) => {
     const [input, setInput] = useState('');
+    const [comments, setComments] = useState([]);
+    const [addView, setAddView] = useState(false);
+
+    useEffect(() => {
+        getComments(props.recipe.length > 0 && props.recipe[0].recipe_id)
+    }, []);
 
     const getComments = (recipeId) => {
+        axios.get(`/api/comments/${recipeId}`)
+            .then(res => {
+                setComments(res.data);
+            })
+            .catch(err => console.log(err));
+    }
+
+    const addComment = (recipeId) => {
+        const {user_id} = props.user;
+
+        axios.post(`/api/comment/${recipeId}`, {content: input, userId: user_id})
+            .then(() => {
+                getComments(props.recipe.length > 0 && props.recipe[0].recipe_id);
+                setInput('');
+            })
+            .catch(err => console.log(err));
+    }
+
+    const editComment = () => {
 
     }
 
+    const deleteComment = () => {
+
+    }
+
+    const toggleView = () => {
+        setAddView(!addView);
+    }
+
+    const handleAdd = () => {
+        addComment(props.recipe.length > 0 && props.recipe[0].recipe_id);
+        toggleView();
+    }
+
+    let mappedComments = comments.map((el, i) => {
+        return <section key={i}>
+            <aside>
+                <img src={el.profile_pic} alt={el.username}/>
+                <p>{el.username}</p>
+            </aside>
+            <main>
+                <p>{el.date_created}</p>
+                <p>{el.content}</p>
+            </main>
+        </section>
+    })
+
     return (
         <section>
-            <section>
-                <h3>Comments</h3>
-                <p>Feel free to share memories or variations of the recipe</p>
-            </section>
-            <button>Add Comment</button>
+            {props.user.user_id 
+                ? 
+                (
+                    <div>
+                        <header>
+                            <section>
+                                <h3>Comments</h3>
+                                <p>Feel free to share memories or variations of the recipe</p>
+                            </section>
+                            {!addView 
+                                ? 
+                                (
+                                    <button onClick={toggleView}>Add Comment</button>
+                                ) 
+                                : 
+                                (
+                                    <section>
+                                        <textarea maxLength={250}
+                                                  value={input}
+                                                  placeholder='Enter comment here'
+                                                  onChange={(e) => setInput(e.target.value)}></textarea>
+                                        <button onClick={handleAdd}>Submit</button>
+                                    </section>
+                                )}
+                        </header>
+                        <main>
+                            {mappedComments}
+                        </main>
+                    </div>
+                ) 
+                : 
+                (
+                    <p>Please login to view Comments</p>
+                )}
         </section>
     )
 }
